d1b13fda81f4d78bd43d90308c09c41295795f7b
Nicoh67 <you@example.com>
2024-10-28 23:49:15 +0100

    génération install-certs.sh

--- traefik-dashboard/server.js
+++ traefik-dashboard/server.js
@@ -56,6 +56,55 @@ async function fetchTraefikRoutes() {
   }
 }
 
+
+// Générer la commande mkcert
+async function generateMkcertCommand() {
+  const nginxDomains = getNginxSites();
+  const phpVersions = getPhpVersionsFromNginxConfig();
+  phpVersions.unshift("");
+  const traefikDomains = await fetchTraefikRoutes();
+
+  const mkcertDomains = ['localhost', 'dev.localhost', '"*.dev.localhost"'];
+
+  // Ajouter les domaines php versions NGINX
+  // mkcertDomains.push(`"*.devphp.localhost"`);
+  phpVersions.forEach(version => {
+    mkcertDomains.push(`"*.devphp${version}.localhost"`);
+
+    // Ajouter les sous-domaines NGINX
+    nginxDomains.forEach(domain => {
+      const subsubDomainsPath = path.join(sitesPath, domain);
+      if (fs.existsSync(subsubDomainsPath)) {
+        const subsubDomains = fs.readdirSync(subsubDomainsPath).filter(file => 
+          fs.statSync(path.join(subsubDomainsPath, file)).isDirectory()
+        );
+        subsubDomains.forEach(subsubDomain => {
+          mkcertDomains.push(`"${subsubDomain}.${domain}.devphp${version}.localhost"`);
+        });
+      }
+     
+      // mkcertDomains.push(`"*.${domain}.devphp.localhost"`);
+      // phpVersions.forEach(version => mkcertDomains.push(`"*.${domain}.devphp${version}.localhost"`));
+    });
+
+  });
+  
+
+
+  // Ajouter les domaines Traefik
+  mkcertDomains.push(...traefikDomains.map(domain => `"${domain}"`));
+
+  // Créer la commande
+  const mkcertCommand = `mkcert --install -cert-file certs/local-cert.pem -key-file certs/local-key.pem ${mkcertDomains.join(" ")}`;
+
+  // Enregistrer la commande dans un fichier generate_certs.sh
+  fs.writeFileSync('/install-certs.sh', `#!/bin/bash\n${mkcertCommand}\n`, { mode: 0o755 });
+  console.log("La commande mkcert a été enregistrée dans generate_certs.sh");
+
+}
+
+
+
 // Route principale pour afficher les liens
 app.get("/", async (req, res) => {
   const routes = await fetchTraefikRoutes();
@@ -68,4 +117,5 @@ app.get("/", async (req, res) => {
 // Démarrage du serveur
 app.listen(port, () => {
   console.log(`Serveur en écoute sur http://localhost:${port}`);
+  generateMkcertCommand();
 });
