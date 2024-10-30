6538c608558b0daa1b7d66bfc6c7a70e710b7658
Nicoh67 <you@example.com>
2024-10-30 23:54:27 +0100

    génération auto des versions PHP NGINX - ajout des version PHP 5.6, 7.4, 8.0, 8.2

--- traefik-dashboard/server.js
+++ traefik-dashboard/server.js
@@ -1,3 +1,4 @@
+const Docker = require('dockerode');
 const express = require("express");
 const axios = require("axios");
 const fs = require('fs');
@@ -5,12 +6,110 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const docker = new Docker();  // Assurez-vous que Docker est accessible sans sudo
+
 const sitesPath = '/var/www/sites'; // Remplace par le chemin correct vers ton dossier des sites
 const nginxConfigPath = '/etc/nginx/nginx.conf'; // Chemin vers votre fichier nginx.conf
+const outputDir = '/etc/nginx/conf.d';
 
 // Configure EJS comme moteur de vue
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+
+
+function writeNginxConf(path, phpVersion, serviceName, confFile) {
+  if(typeof confFile == "undefined")
+    confFile = serviceName;
+
+  const conf = `
+server {
+    listen 80;
+    server_name ~^(?<subdomain>.+)\.devphp${phpVersion}.localhost$;
+
+    root /var/www/html/$subdomain;
+    index index.php;
+
+    location ~ \\.php$ {
+        include fastcgi_params;
+        fastcgi_pass ${serviceName}:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    }
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+}`;
+
+  // Écrire la configuration dans un fichier
+  const confPath = path.join(outputDir, `nginx_${confFile}.conf`);
+  fs.writeFileSync(confPath, conf, 'utf8');
+  console.log(`Configuration NGINX générée pour ${confFile} dans ${confPath}`);
+
+}
+
+// Fonction générant les configs de nginx à partir des versions de PHP isntallées
+async function generateConfigsFromDocker() {
+  const versions = await getPhpVersionsFromDocker();
+  let latestVersion = versions[versions.length-1];
+  
+
+  try {
+    const containers = await docker.listContainers();
+
+    containers.forEach(async (containerInfo) => {
+      const container = docker.getContainer(containerInfo.Id);
+      const data = await container.inspect();
+      
+      // Vérifier si l'image du conteneur est PHP
+      if (data.Config.Image.startsWith('php:')) {
+        const phpVersion = (data.Config.Image.split(':')[1] || 'default').replace('-fpm', '').replace('.', '');
+        const serviceName = data.Name.replace(/^\//, '');  // Retirer le slash du début
+        
+        // Si c'est la dernière version PHP installée, on la choisi par défaut
+        if(phpVersion == latestVersion)
+          writeNginxConf(path, "", serviceName, "");
+
+        writeNginxConf(path, phpVersion, serviceName);
+      }
+    });
+  } catch (error) {
+    console.error("Erreur lors de la génération des configurations NGINX:", error);
+  }
+}
+
+
+// Fonction pour extraire les versions de PHP de nginx.conf
+async function getPhpVersionsFromDocker() {
+  let versions = [];
+
+  try {
+    const containers = await docker.listContainers();
+
+    // Utiliser Promise.all pour attendre que toutes les promesses soient résolues
+    const versionPromises = containers.map(async (containerInfo) => {
+      const container = docker.getContainer(containerInfo.Id);
+      const data = await container.inspect();
+
+      // Vérifier si l'image du conteneur est PHP
+      if (data.Config.Image.startsWith('php:')) {
+        let version = (data.Config.Image.split(':')[1] || 'default').replace('-fpm', '').replace('.', '');
+        return version; // Retourner la version pour cet élément
+      }
+      return null; // Retourner null si ce n'est pas une image PHP
+    });
+
+    // Récupérer les résultats des promesses, puis filtrer les nulls
+    const results = await Promise.all(versionPromises);
+    versions = results.filter(version => version !== null);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des versions PHP :", error);
+  }
+
+  return [...new Set(versions)].sort((a, b) => a - b); // Rendre unique et trier
+}
+
 
 // Fonction pour lire les dossiers de nginx
 function getNginxSites() {
@@ -60,7 +159,7 @@ async function fetchTraefikRoutes() {
 // Générer la commande mkcert
 async function generateMkcertCommand() {
   const nginxDomains = getNginxSites();
-  const phpVersions = getPhpVersionsFromNginxConfig();
+  const phpVersions = await getPhpVersionsFromDocker();
   phpVersions.unshift("");
   const traefikDomains = await fetchTraefikRoutes();
 
@@ -107,15 +206,18 @@ async function generateMkcertCommand() {
 
 // Route principale pour afficher les liens
 app.get("/", async (req, res) => {
+  generateMkcertCommand();
   const routes = await fetchTraefikRoutes();
   const routes_nginx = getNginxSites();
   // const routes2 = getNginxSites().map(site => (`${site}.devphp.localhost`));
-  const phpVersions = getPhpVersionsFromNginxConfig(); // Obtenez les versions de PHP
+  const phpVersions = await getPhpVersionsFromDocker(); // Obtenez les versions de PHP
+  // const phpVersions = getPhpVersionsFromNginxConfig(); // Obtenez les versions de PHP
   res.render('index', { routes, routes_nginx, phpVersions }); // Rendre le fichier EJS
 });
 
 // Démarrage du serveur
 app.listen(port, () => {
   console.log(`Serveur en écoute sur http://localhost:${port}`);
+  generateConfigsFromDocker();
   generateMkcertCommand();
 });
